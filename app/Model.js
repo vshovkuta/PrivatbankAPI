957e9515ffad44e62f279e1f1598b5d7202b1dbd
Volodymyr Shovkuta <47761397+vshovkuta@users.noreply.github.com>
2019-09-23 10:08:46 +0300

    Update Model.js

--- app/Model.js
+++ app/Model.js
@@ -2,15 +2,14 @@ export class Model {
   constructor(controller) {
     this.controller = controller;
     this.baseApiURL = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
-
-    this.getValue();
-    };
+    this.updatedTime;
+  };
 
     getValue() {
       fetch(this.baseApiURL).then((result) => result.json())
                             .then((resultJSON) => {
-                              this.controller.updateData(resultJSON);
+                              this.updatedTime = new Date();
+                              this.controller.updateData(resultJSON, this.updatedTime);
                             });
     }
-
 }
